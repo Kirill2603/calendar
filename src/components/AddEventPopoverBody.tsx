676d473ca50b3df4cea217df68713106b6aa270d
kirill2603 <k.radobolsky@gmail.com>
2022-07-21 12:45:56 +0300

    add color palette

--- src/components/AddEventPopoverBody.tsx
+++ src/components/AddEventPopoverBody.tsx
@@ -1,15 +1,58 @@
-import React from 'react'
-import { Button, Checkbox, Flex, HStack, IconButton, Input, PopoverBody, Textarea } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {
+  Button,
+  Checkbox,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  PopoverBody,
+  PopoverTrigger,
+  Textarea,
+  Grid,
+  MenuList, MenuItem, Box, Popover, PopoverContent, PopoverHeader, PopoverArrow,
+} from '@chakra-ui/react'
 import { ArrowForwardIcon, ChatIcon, Icon, StarIcon } from '@chakra-ui/icons'
-import { FaClock, FaMapMarkerAlt } from 'react-icons/fa'
+import { FaCircle, FaClock, FaMapMarkerAlt } from 'react-icons/fa'
+import { MdOutlineColorLens } from 'react-icons/md'
 
 export const AddEventPopoverBody = () => {
+
+  const [addEventState, setAddEventState] = useState({
+    title: '',
+    description: '',
+    allDay: false,
+    start: 0,
+    end: 0,
+    isDone: false,
+  })
+
   return (
     <PopoverBody>
       <HStack pb={2}>
-        <IconButton ml={6} aria-label='Search database' variant='outline' icon={<StarIcon />} />
+        <Box>
+          <Popover placement='bottom-start'>
+            <PopoverTrigger>
+              <IconButton aria-label='color' variant='outline' icon={<MdOutlineColorLens />} />
+            </PopoverTrigger>
+            <PopoverContent  w='fit-content'>
+              <PopoverHeader fontWeight='bold'>Color</PopoverHeader>
+              <PopoverArrow />
+              <Grid templateColumns='repeat(3, 1fr)'>
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='red' />} />
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='green' />} />
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='blue' />} />
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='purple' />} />
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='orange' />} />
+                <IconButton as={'li'} m={2} aria-label='red' icon={<FaCircle fill='yellow' />} />
+              </Grid>
+            </PopoverContent>
+          </Popover>
+        </Box>
         <Input variant='outline' placeholder='Title' />
-        <Checkbox whiteSpace='nowrap'>All Day</Checkbox>
+        <Checkbox whiteSpace='nowrap' isChecked={addEventState.allDay}>All Day</Checkbox>
       </HStack>
       <HStack pb={2}>
         <ChatIcon />
